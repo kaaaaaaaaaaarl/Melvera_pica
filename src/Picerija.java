6758fbc7c58cae4012427e3f45dca32d24c7bd9d
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-01-26 08:48:58 +0200

    start2

--- src/Picerija.java
+++ src/Picerija.java
@@ -1,2 +1,25 @@
+import javax.swing.*;
 public class Picerija {
+
+    public static void main(String args[]){
+        String[] opcijas = {"Pasūtīt", "veidot savu picu", "vel informacija", "cenas", "Neko, pārdomāju!"};
+        String getFavFruit = (String) JOptionPane.showInputDialog(
+                null,
+                "Laipni lūgti pcas veikalā",
+                "Ko darīt?",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcijas,
+                opcijas[3]);
+
+
+
+
+
+
+
+
+    }
+
+
 }
