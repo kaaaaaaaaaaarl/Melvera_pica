7c76d3850a698e61fc8a5cc55b544719307e54e3
kaaaaaaaaaaarl <caalis18@gmail.com>
2022-01-27 11:29:18 +0200

    pirms Print

--- src/Picerija.java
+++ src/Picerija.java
@@ -1,8 +1,63 @@
 import javax.swing.*;
+import java.util.ArrayList;
 public class Picerija {
+    private static void pasutit(double cena, ArrayList pasūtījums){
+
+        String[] opcijas1 = {"peperoni +2.50", "hawaii + 3.50", "4 sezonu +4.50", "italiago + 2.50","Pica proscitto +3.00", "Neko, pārdomāju!"};
+        String pzzaa = (String) JOptionPane.showInputDialog(
+                null,
+                "Laipni lūgti pcas veikalā",
+                "Ko darīt?",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcijas1,
+                opcijas1[3]);
+        switch (pzzaa) {
+            case "peperoni +2.50":
+                cena = cena+ 2.50;
+                pasūtījums.add("peperoni");
+                break;
+            case "hawaii + 3.50":
+                cena = cena+ 3.50;
+                pasūtījums.add("hawaii");
+                break;
+            case "4 sezonu +4.50":
+                pasūtījums.add("4 sezonu");
+                cena = cena+ 4.50;
+                break;
+            case "italiago + 2.50":
+                cena = cena+ 2.50;
+                pasūtījums.add("italiago");
+                break;
+            case "Pica proscitto +3.00":
+                cena = cena+ 3;
+                pasūtījums.add("Pica proscitto");
+                break;
+            case "Neko, pārdomāju!":
+
+
+                break;}}
+
+
+
 
     public static void main(String args[]){
-        String[] opcijas = {"Pasūtīt", "veidot savu picu", "vel informacija", "cenas", "Neko, pārdomāju!"};
+        double cena = 0;
+        boolean repeat = true;
+        ArrayList<String> jaunaPica = new ArrayList<String>();
+        ArrayList<String> pasūtījums = new ArrayList<String>();
+
+
+
+
+
+
+
+
+
+
+        do {
+        String[] opcijas = {"Pasūtīt", "veidot savu picu", "cenas","gatavs, pasūtīt!", "Neko, pārdomāju!"};
         String getFavFruit = (String) JOptionPane.showInputDialog(
                 null,
                 "Laipni lūgti pcas veikalā",
@@ -13,8 +68,30 @@ public class Picerija {
                 opcijas[3]);
 
 
+                switch (getFavFruit) {
+                    case "Pasūtīt":
+                        pasutit(cena, pasūtījums);
+                        break;
+                    case "veidot savu picu":
+                        jaunaPica = taisit.newPica(cena);
+                        break;
+
+                    case "cenas":
+                        JOptionPane.showMessageDialog(null, "Jums šobrīd būtu jāmaksā: "+cena);
+                        break;
+                    case "gatavs, pasūtīt!":
 
 
+                        break;
+                    case "Neko, pārdomāju!":
+                        repeat=true;
+                        break;
+                    default:
+
+                }
+
+
+            }while(repeat==true);
 
 
 
@@ -22,4 +99,7 @@ public class Picerija {
     }
 
 
-}
+    }
+
+
+
---
+++ src/taisit.java
@@ -0,0 +1,132 @@
+import javax.swing.*;
+import java.util.ArrayList;
+
+
+
+public class taisit {
+
+
+    static boolean izv= false;
+    public static ArrayList newPica (double cena){
+        String[] opcijas = {"Mozzarella +0.20", "Cheddar +0.25", "Gorgonzola +0.30", "Provolone +0.30", "bez siera"};
+        String siers = (String) JOptionPane.showInputDialog(
+                null,
+                "kādu sieru?",
+                "Jūsu paša recepte?",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcijas,
+                opcijas[3]);
+        ArrayList<String> jaunaPica = new ArrayList<String>();
+
+        switch (siers) {
+            case "Mozzarella +0.20":
+                jaunaPica.add("Mozzarella");
+                cena =+ 0.2;
+                break;
+            case "Cheddar +0.25":
+                jaunaPica.add("Cheddar");
+                cena =+ 0.25;
+                break;
+            case "Gorgonzola +0.30":
+                jaunaPica.add("Gorgonzola");
+                cena =+ 0.3;
+                break;
+            case "Provolone +0.30":
+                jaunaPica.add("Provolone");
+                cena =+ 0.3;
+                break;
+            case "bez siera":
+                jaunaPica.add("bez siera");
+                break;
+
+
+
+    }
+
+    do {
+        String[] opcijas1 = {"peperoni +0.20", "italijas desa +0.25", "Malta liellopa gaļa +0.50", "bekons +0.50", "bez gaļas"};
+        String gala = (String) JOptionPane.showInputDialog(
+                null,
+                "kādu gaļu?",
+                "Jūsu paša recepte?",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcijas1,
+                opcijas1[3]);
+
+        switch (gala) {
+            case "peperoni +0.20":
+                jaunaPica.add("peperoni");
+                cena = +0.2;
+                break;
+            case "italijas desa +0.25":
+                jaunaPica.add("italijas desa");
+                cena = +0.25;
+                break;
+            case "Malta liellopa gaļa +0.50":
+                jaunaPica.add("Malta liellopa gaļa");
+                cena = +0.5;
+                break;
+            case "bekons +0.50":
+                jaunaPica.add("bekons");
+                cena = +0.5;
+                break;
+            case "bez gaļas":
+
+                break;
+
+
+        }
+
+        String[] opcijas2 = {"sēnes +0.15", "chilli +0.45", "tomāti +0.15", "olives +0.30", "bez darzeņiem"};
+        String galaa = (String) JOptionPane.showInputDialog(
+                null,
+                "Jūsu paša recepte",
+                "kādu sieru?",
+                JOptionPane.QUESTION_MESSAGE,
+                null,
+                opcijas2,
+                opcijas2[3]);
+
+        switch (gala) {
+            case "sēnes +0.15":
+                jaunaPica.add("peperoni");
+                cena = +0.15;
+                break;
+            case "chilli +0.45":
+                jaunaPica.add("italijas desa");
+                cena = +0.45;
+                break;
+            case "tomāti +0.15":
+                jaunaPica.add("Malta liellopa gaļa");
+                cena = +0.15;
+                break;
+            case "olives +0.30":
+                jaunaPica.add("bekons");
+                cena = +0.30;
+                break;
+            case "bez darzeņiem":
+
+                break;
+
+
+        }
+
+
+
+        int dialogButton = JOptionPane.YES_NO_OPTION;
+        int dialogResult = JOptionPane.showConfirmDialog (null, "Vai vēlaties vel uzlikt uz savas picas?"," ",dialogButton);
+        if(dialogResult == JOptionPane.YES_OPTION){
+            izv= true;
+        }else{izv=false;}
+
+    }while(izv==true);
+    return(jaunaPica);
+
+
+
+
+
+
+}}
